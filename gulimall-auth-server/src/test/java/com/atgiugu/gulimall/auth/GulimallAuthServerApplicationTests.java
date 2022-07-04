0db276ed1bf611aaceb88a64366d9b6075596be3
zhangqingnan <825973306@qq.com>
2022-07-04 14:28:11 +0800

    购物车功能完成

---
+++ gulimall-auth-server/src/test/java/com/atgiugu/gulimall/auth/GulimallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atgiugu.gulimall.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
